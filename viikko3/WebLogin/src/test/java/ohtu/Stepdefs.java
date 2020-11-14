b05dfe5230b759c94c6503182dd03f39676aa4e2
Antti Laakso <antti@ninchat.com>
2020-11-14 22:43:06 +0200

    Teht 9: remove antipattern

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -42,18 +42,8 @@ public class Stepdefs {
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
-    }    
-    
-    @When("username {string} and password {string} are given")
-    public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
-        logInWith(username, password);
-    }   
-    
-    @Then("system will respond {string}")
-    public void systemWillRespond(String pageContent) throws Throwable {
-        assertTrue(driver.getPageSource().contains(pageContent));
     }
-    
+
     @After
     public void tearDown(){
         driver.quit();
